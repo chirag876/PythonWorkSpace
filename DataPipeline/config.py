8af7db53ed2b8ef574822e9c31c1a456707dc231
Chirag Gupta <108919262+chirag876@users.noreply.github.com>
2024-06-23 12:05:40 +0530

    Delete DataPipeline directory

--- DataPipeline/config.py
+++
@@ -1,9 +0,0 @@
-mongodb_uri='mongodb+srv://Project0:Diagnostics11@cluster0.w9m3hlk.mongodb.net/'#'mongodb://localhost:27017/'#
-db='stage_0'
-db_stage0='new_stage_0'
-connectionstring='DefaultEndpointsProtocol=https;AccountName=staiblobstorage;AccountKey=tLuUgLKixZwC6oTfmRAB0kZpaOTieFiXCd+Gn5NdBSu98h88AJKynR03M1t670+RzrJy1QA4+Miy+AStICcicA==;EndpointSuffix=core.windows.net'
-containername = 'csvcontainer'
-sourcetostage0configs='./Configs/MongoDB_Configs'
-stage0toazureconfigs='./Configs/Blob_Configs'
-csvfilepath='./Configs/MongoDB_tables'
-blobcsvpath='./Configs/Blob_CSV'
